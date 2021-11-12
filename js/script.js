a713e5c837866016729adee542bb420db0bb570d
LarisaGimbu <gimbularisa@gmail.com>
2021-11-12 14:04:00 +0100

    insertToDo

--- js/script.js
+++ js/script.js
@@ -6,11 +6,15 @@
   2. Stampare all’interno di una lista, un item per ogni todo.
   Se la proprietà `done` è uguale a `true`, visualizzare il testo del todo ~~sbarrato~~.
 
-  3.Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+  3. Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+
+  4. Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 */
 
 const app = new Vue({
+  
   el:'#app',
+
   data:{
     toDoList:[
       {
@@ -30,12 +34,21 @@ const app = new Vue({
         done: false,
       },
     ],
+    newToDo:
+    {
+      text:'',
+      done: false,
+    },
   },
 
   methods:{
 
     removeItem(index){
       this.toDoList.splice(index, 1);
+    },
+
+    insertToDo(){
+      this.toDoList.push(this.newToDo);
     }
   }
 })
